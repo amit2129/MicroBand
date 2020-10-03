219271179883ff64b9af5f48fae93569c7d7361e
Amit Matityahu <amit2129@gmail.com>
2020-10-03 13:13:00 +0300

    [Feature] Added raw protocol utilities
    Added the MicroBand raw protocol utilities of receiving (mostly)
    and some send utilities
    
    Signed-off-by: Amit Matityahu <amit2129@gmail.com>

---
+++ raw_proto/src/mb_transport_shared.h
@@ -0,0 +1,16 @@
+#ifndef _MB_TRANSPORT_SHARED
+#define _MB_TRANSPORT_SHARED
+
+#define ETH_P_MB 0x2129
+
+typedef struct microband_transport_header {
+	uint32_t source_qp;
+	uint32_t dest_qp;
+	uint32_t sequence_number;
+	uint32_t ack_number;
+	uint16_t data_len;
+	uint32_t reserved_1;
+	uint16_t reserved_2;
+} mb_transport;
+
+#endif

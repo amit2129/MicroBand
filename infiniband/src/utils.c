4525e839d3312e7c7458b25ff56f2bec6ae48a9f
Amit Matityahu <amit2129@gmail.com>
2020-10-05 01:51:14 +0300

    temp commit before app dev
    Signed-off-by: Amit Matityahu <amit2129@gmail.com>

---
+++ infiniband/src/utils.c
@@ -0,0 +1,15 @@
+#include "utils.h"
+#include <stdio.h>
+
+//void print_mr(MR *mr) {
+//	int i;
+//	for (i = 0; i < mr->sz; i++)
+//	{
+//	    if (i > 0)
+//		    printf(":");
+//	    else
+//		    printf("\n");
+//	    printf("%02X", mr->buffer[i]);
+//	}
+//	printf("\n");
+//}
